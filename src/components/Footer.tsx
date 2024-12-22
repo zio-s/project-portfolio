9e7e8c30243e33c792b35a644c55bed08e98d253
seming2 <popqr3@naver.com>
2024-12-22 22:49:57 +0900

    footer 픽스

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='text-center py-4 text-sm'>
+    <footer className='fixed bottom-0 z-50 left-1/2 -translate-x-1/2 text-center py-4 text-sm'>
       <p>Thanks for stopping by!</p>
     </footer>
   );
