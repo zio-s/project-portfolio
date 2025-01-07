b75aa6d57bf9eb681d512ca94e14f04b71b0c203
seming2 <popqr3@naver.com>
2025-01-07 23:38:16 +0900

    header 적용된 해시 감지 로직 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,54 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Footer = () => {
+  const [textColor, setTextColor] = useState('var(--color3)');
+
+  // 색상 업데이트 함수
+  const updateTextColor = (hash: string) => {
+    setTextColor(hash !== '' ? '#263c4f' : 'var(--color3)');
+  };
+
+  useEffect(() => {
+    // 초기 해시 체크
+    updateTextColor(window.location.hash);
+
+    // 해시 변경 감지
+    const handleHashChange = () => {
+      updateTextColor(window.location.hash);
+    };
+
+    // URL 변경 감지
+    const handleUrlChange = () => {
+      updateTextColor(window.location.hash);
+    };
+
+    window.addEventListener('hashchange', handleHashChange);
+    window.addEventListener('popstate', handleUrlChange);
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+      window.removeEventListener('popstate', handleUrlChange);
+    };
+  }, []);
+
   return (
-    <footer className='fixed flex items-center justify-between w-full p-8  bottom-0 z-50 left-1/2 -translate-x-1/2 text-center py-4 text-sm '>
-      <p className='my_text' style={{ color: 'var(--color3)' }}>
+    <footer className='fixed flex items-center justify-between w-full p-8 bottom-0 z-50 left-1/2 -translate-x-1/2 text-center py-4 text-sm'>
+      <p className='my_text' style={{ color: textColor }}>
         This is just the beginning, stay tuned!
       </p>
       <div className='sns'>
         <ul className='flex gap-3'>
           <li className='sns_git'>
             <Link href='https://github.com/zio-s' target='_blank'>
-              <i className='la la-git' style={{ fontSize: '32px', color: 'var(--color3)' }}></i>
+              <i className='la la-git' style={{ fontSize: '32px', color: textColor }}></i>
             </Link>
           </li>
           <li className='sns_blog'>
             <Link href='https://github.com/zio-s' target='_blank'>
-              <i className='lab la-blogger-b' style={{ fontSize: '32px', color: 'var(--color3)' }}></i>
+              <i className='lab la-blogger-b' style={{ fontSize: '32px', color: textColor }}></i>
             </Link>
           </li>
         </ul>
